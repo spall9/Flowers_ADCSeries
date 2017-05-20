b2f151072375c42fa81286b148bce0f3508757e1
NightMoon032 <1076751236@qq.com>
2017-05-20 15:27:35 +0800

    V5

--- Flowers_Utility_Loader/Program.cs
+++ Flowers_Utility_Loader/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Flowers_ADCSeries_Loader
+﻿namespace Flowers_Utility_Loader
 {
     using EloBuddy.SDK.Events;
 
@@ -9,8 +9,7 @@
     internal class Program
     {
         private static readonly string getAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-        private static readonly string dllPath = Path.Combine(getAppDataPath, @"EloBuddy\Addons\Libraries\MoonAIO.dll");
-
+        private static readonly string dllPath = Path.Combine(getAppDataPath, @"EloBuddy\Addons\Libraries\MoonBrain.dll");
         private static void Main(string[] eventArgs)
         {
             Loading.OnLoadingComplete += Args =>
@@ -20,14 +19,14 @@
                     File.Delete(dllPath);
                 }
 
-                var prdll = Properties.Resources.Flowers__ADC_Series;
+                var prdll = Properties.Resources.Flowers__Utility;
                 using (var fs = new FileStream(dllPath, FileMode.Create))
                 {
                     fs.Write(prdll, 0, prdll.Length);
                 }
 
                 var dllpath = Assembly.LoadFrom(dllPath);
-                var main = dllpath.GetType("Flowers_ADCSeries.MyLoader").GetMethod("Init");
+                var main = dllpath.GetType("Flowers_Utility.MyLoader").GetMethod("Init");
                 main.Invoke(null, null);
             };
         }
