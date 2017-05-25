140c8e27d4079bd5f8cbfea0d4af7d5db59ca838
NightMoon032 <1076751236@qq.com>
2017-05-25 22:41:04 +0800

    Easy躲避测试版

---
+++ EasyEvade_Loader/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace EasyEvade_Loader
+{
+    using EloBuddy.SDK.Events;
+
+    using System;
+    using System.IO;
+    using System.Reflection;
+
+    internal class Program
+    {
+        private static readonly string getAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        private static readonly string dllPath = Path.Combine(getAppDataPath, @"EloBuddy\Addons\Libraries\MoonEvade.dll");
+
+        private static void Main(string[] eventArgs)
+        {
+            Loading.OnLoadingComplete += Args =>
+            {
+                if (File.Exists(dllPath))
+                {
+                    File.Delete(dllPath);
+                }
+
+                var prdll = Properties.Resources.EasyEvade;
+                using (var fs = new FileStream(dllPath, FileMode.Create))
+                {
+                    fs.Write(prdll, 0, prdll.Length);
+                }
+
+                var dllpath = Assembly.LoadFrom(dllPath);
+                var main = dllpath.GetType("EasyEvade.MyLoader").GetMethod("Init");
+                main.Invoke(null, null);
+            };
+        }
+    }
+}
